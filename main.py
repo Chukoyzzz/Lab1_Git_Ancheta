5684bde929e48fac2ef747de0bf35c8d91412297
Chukoyzzz <williamancheta321@gmail.com>
2026-02-02 10:07:32 +0800

    Added Section info for Ancheta, William Gwyneth R..

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Lab 1: Version Control")
 print("Lab 1: Author [Ancheta, William Gwyneth R.]")
 print("Student ID: TUPM-25-0601")
+
+print("Lab 1: Version Control")
+print("Author: Ancheta, William Gwyneth R.")
+print("Class Section: [1C]")
+#
